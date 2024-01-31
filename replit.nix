4504f1fa931efa49701f326834b7377a964305b3
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2024-01-31 20:07:01 +0100

    2024-01-31

--- replit.nix
+++ replit.nix
@@ -1,18 +1,20 @@
 { pkgs }: {
   deps = [
-    # Rust
-    pkgs.rustc
-	pkgs.rustfmt
-	pkgs.cargo
-	pkgs.cargo-edit
-    pkgs.cargo-watch
-    pkgs.rust-analyzer
-
     # Python
     pkgs.python312
     pkgs.poetry
+    pkgs.python311Packages.fastjsonschema
     pkgs.pipx
     pkgs.black
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
 
     # Utilities
     pkgs.htop
